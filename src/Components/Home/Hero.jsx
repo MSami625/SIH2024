85f98c045231c9ec08931f1cd4684a837ff776df
M Sami <beboolean@gmail.com>
2024-08-29 13:12:23 +0530

    Services and Faq Components Done

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({heroData}) => {
   return (
     <>
-      <div className="w-full flex flex-col lg:flex-row items-start py-6 gap-10 text-white relative">
+      <div className="w-full flex flex-col lg:flex-row items-start py-6 gap-10 text-white relative mb-96">
         <div className="flex flex-col items-start w-full lg:w-[50%]">
           <span className="sm:text-[8vw] md:text-[8vw] lg:text-[4.5vw] text-[11vw] font-semibold pt-10 leading-tight">
-            Write a Title Here. Click to Add and Edit Title Text.
+            {heroData[0].heading}
           </span>
           <p className=" text-[1.1rem] sm:text-[1.5rem] pt-5 pb-5 font-light text-justify">
-            This is a space to welcome visitors to the site. Add an engaging
-            image or video.{" "}
+          {heroData[0].subheading}
+         
           </p>
           <button className="rounded-full px-11 py-3 duration-300 cursor-pointer bg-black text-white border-2 hover:border-[#968f8f] hover:bg-white hover:text-black active:scale-[0.95]">
             Click Me
--- src/Components/Home/services.jsx
+++ src/Components/Home/services.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
-function services() {
-  return <div className="bg-"></div>;
+function Services() {
+
+  const servicesData = [
+    {
+      id: 1,
+      title: "Name of Service",
+      description: "This is the space to describe the service and explain how customers or clients can benefit from it. It’s an opportunity to add a short description that includes relevant details, like pricing, duration, location and how to book the service",
+    },
+    {
+      id: 2,
+      title: "Name of Service",
+      description: "This is the space to describe the service and explain how customers or clients can benefit from it. It’s an opportunity to add a short description that includes relevant details, like pricing, duration, location and how to book the service",
+    },
+    {
+      id: 3,
+      title: "Name of Service",
+      description: "This is the space to describe the service and explain how customers or clients can benefit from it. It’s an opportunity to add a short description that includes relevant details, like pricing, duration, location and how to book the service",
+    },
+    {
+      id: 4,
+      title: "Name of Service",
+      description: "This is the space to describe the service and explain how customers or clients can benefit from it. It’s an opportunity to add a short description that includes relevant details, like pricing, duration, location and how to book the service",
+    },
+  
+  ];
+
+  return (
+    <div className="bg-[#ecedf8] pt-20">
+      <div className="flex flex-col justify-center items-start px-[8vw] gap-10 pb-20 border-b-[1px] border-b-black">
+        <div className="px-4 py-2 text-xs font-bold bg-[#2F2B36] tracking-widest text-white border-2 rounded-tr-xl">
+          SERVICES
+        </div>
+        <p className="text-4xl text-neutral-700 font-semibold">Committed to excellence</p>
+      </div>
+      
+      <div className="flex w-full flex-col lg:flex-col">
+        {servicesData.map((service) => (
+          <div key={service.id} className="border-b-[1px] border-b-black">
+          <div className="w-full flex gap-6 items-start lg:gap-48 px-[8vw] py-12 ease-in-out duration-300  flex-col lg:flex-row hover:text-white hover:bg-neutral-500">
+              <span className="text-5xl font-semibold">0{service.id}</span>
+              <h3 className="text-3xl text-center">{service.title}</h3>
+              <p className="text-base leading-7 text-justify">{service.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default services;
+export default Services;
