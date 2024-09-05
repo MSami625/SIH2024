650d6d2f39a14000291337149c3bbb59e569b465
M Sami <beboolean@gmail.com>
2024-09-05 17:18:09 +0530

    fix

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -35,49 +35,51 @@ function Dashboard() {
   const [activeItem, setActiveItem] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
 
-  return (<>
-    <Header/>
-    <div className="w-screen h-screen pl-8 pr-8 pt-4 grid grid-rows-[30%_70%] grid-cols-[20%_80%]">
-      {/* Navbar */}
-      <div
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className={`relative flex flex-col items-center min-h-[80vh] rounded-3xl transition-all duration-300 ease-in-out ${isHovered ? "w-60 " : "w-20"} bg-[rgb(154,91,248)]`}
-      >
-        <div className="flex w-10 flex-col items-center gap-8 mt-6 justify-start hover:text-[rgb(154,91,248)]">
-          {navItems.map((item) => (
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                setActiveItem(item.id);
-              }}
-              key={item.id}
-              className={`flex items-center w-10 max-h-[40px] gap-4 p-2 rounded-2xl cursor-pointer text-white text-lg transition-all duration-300 ease-in-out ${
-                activeItem === item.id
-                  ? "bg-white p-[10px] shadow-xl text-[rgb(154,91,248)]"   
-                  : " bg-transparent hover:bg-white hover:text-[rgb(154,91,248)] text-white"
-              } ${isHovered ? "w-44 " : ""}`}
-            >
-              <a href="#" className={`flex items-center overflow-hidden w-10 ${isHovered ? "w-44" : ""}`}>
-                <div>{item.icon}</div>
-                <span className="ml-2">{item.text}</span>
-              </a>
-            </div>
-          ))}
+  return (
+    <>
+      <Header />
+      <div className="w-screen h-screen pl-8 pr-8 pt-4 grid grid-rows-[30%_70%] grid-cols-[20%_80%]">
+        {/* Navbar */}
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={`relative flex flex-col items-center min-h-[80vh] rounded-3xl transition-all duration-300 ease-in-out ${isHovered ? "w-60" : "w-20"} bg-[rgb(154,91,248)]`}
+        >
+          <div className="flex flex-col items-center gap-8 mt-6 justify-start hover:text-[rgb(154,91,248)]">
+            {navItems.map((item) => (
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  setActiveItem(item.id);
+                }}
+                key={item.id}
+                className={`flex items-center w-10 max-h-[40px] gap-4 p-2 rounded-2xl cursor-pointer text-lg transition-all duration-300 ease-in-out ${
+                  activeItem === item.id
+                    ? "bg-white p-[10px] shadow-xl text-[rgb(154,91,248)]"
+                    : "bg-transparent hover:bg-white hover:text-[rgb(154,91,248)] text-white"
+                } ${isHovered ? "w-44" : ""}`}
+              >
+                <a href="#" className={`flex items-center overflow-hidden ${isHovered ? "w-44" : ""}`}>
+                  <div>{item.icon}</div>
+                  <span className="ml-2">{item.text}</span>
+                </a>
+              </div>
+            ))}
+          </div>
+
+          <div className="absolute bottom-10 w-full flex justify-center text-white text-2xl">
+            <a href="#">
+              <IoLogOutOutline />
+            </a>
+          </div>
         </div>
 
-        <div className="absolute bottom-10 w-full flex justify-center text-white text-2xl">
-          <a href="#">
-            <IoLogOutOutline />
-          </a>
+        {/* Main Content */}
+        <div className="flex flex-col gap-10 items-center justify-center w-full h-full">
+          {navItems[activeItem].display}
         </div>
       </div>
-
-      {/* Main Content */}
-      <div className="flex flex-col gap-10 items-center justify-center w-full h-full">
-        {navItems[activeItem].display}
-      </div>
-    </div></>
+    </>
   );
 }
 
